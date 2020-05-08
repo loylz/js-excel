b23697165547d219e8a5c76642bdcf6ee202e1d9
Aleksey <alleks6@gmail.com>
2020-05-08 23:45:05 +0300

    save

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const path = require('path');
+
 module.exports = {
-    
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        filename: 'bandle.[hash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@core': path.resolve(__dirname, 'src/core')
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new CopyPlugin([
+            {
+                from: path.resolve(__dirname, 'src/NaN.ico'),
+                to: path.resolve(__dirname, 'dist')
+            }, 
+        ]
+        ),
+        new MiniCssExtractPlugin({
+            filename: 'bandle.[hash].css'
+        })
+    ]
 }
